2fbc233c293978250c67b79306ae3a2fdc90c817
Ruby McCarthy <907ruby@gmail.com>
2019-08-25 17:45:46 -0700

    Single Page Applications Part 2 Menu links fixed

--- src/components/MenuComponents.js
+++ src/components/MenuComponents.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import {
-  Card, CardImg, CardImgOverlay,
-  CardTitle, Breadcrumb, BreadcrumbItem
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 function RenderMenuItem({ dish, onClick }) {
   return (
     <Card>
-      <Link to={`/menu/${dish.id}`} >
+      <Link to={`/menu/${dish.id}`}>
         <CardImg width="100%" src={dish.image} alt={dish.name} />
         <CardImgOverlay>
           <CardTitle>{dish.name}</CardTitle>
@@ -18,8 +22,8 @@ function RenderMenuItem({ dish, onClick }) {
   );
 }
 
-const Menu = (props) => {
-  const menu = props.dishes.map((dish) => {
+const Menu = props => {
+  const menu = props.dishes.map(dish => {
     return (
       <div className="col-12 col-md-5 m-1" key={dish.id}>
         <RenderMenuItem dish={dish} />
@@ -31,7 +35,9 @@ const Menu = (props) => {
     <div className="container">
       <div className="row">
         <Breadcrumb>
-          <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+          <BreadcrumbItem>
+            <Link to="/home">Home</Link>
+          </BreadcrumbItem>
           <BreadcrumbItem active>Menu</BreadcrumbItem>
         </Breadcrumb>
         <div className="col-12">
@@ -39,9 +45,7 @@ const Menu = (props) => {
           <hr />
         </div>
       </div>
-      <div className="row">
-        {Menu}
-      </div>
+      <div className="row">{menu}</div>
     </div>
   );
 }
