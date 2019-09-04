9bc2073ee8c54af191dc9e40e853e0e0954ccfe6
Ruby McCarthy <907ruby@gmail.com>
2019-09-04 11:29:58 -0700

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -18,6 +18,7 @@ import {
 } from "reactstrap";
 import { LocalForm, Control, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 function RenderDish({ dish }) {
   if (dish != null)
@@ -64,7 +65,25 @@ function RenderComments({ comments, addComment, dishId }) {
   else return <div />;
 }
 const Dishdetail = props => {
-  if (props.dish != null) {
+  if (props.isLoading) {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <Loading />
+        </div>
+      </div>
+    )
+  }
+  else if (props.errMess) {
+    return (
+      <div className='container'>
+        <div classNmae='row'>
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
+    );
+  }
+  else if (props.dish != null) {
     return (
       <div className="container">
         <div className="row">
